a66ad2de6edd34024e03f74739df2a0eaefb8320
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-02-14 15:30:49 +0000

    Add method to comment on a product

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from versus_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
+    serializer_class = CommentSerializer
+    # we don't want anonymous users to comment
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
