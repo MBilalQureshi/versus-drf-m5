6699a191f9e7e587cde7fbc279ac2a9f1300f22a
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-02-15 22:58:19 +0000

    Add proxy and create axiosDefault for future reference

---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,14 @@
+/*
+Your code is now set up so that you can follow along with what you learned in the Moments walkthrough. 
+There is one final difference between the Moments walkthrough setup and what you will need for this combined workspace. 
+That is you will not need the BaseURL setting in the axiosDefaults.js file when it is created. 
+This is because the combined workspace will receive the JSON from the API from the same URL,
+ just on a different port (which was set with the proxy above).
+
+// IMPORTANT!!
+ // Because this React app is running in the same workspace as the API,
+
+ // there is no need to set a separate baseURL until you reach deployment.
+
+ // Setting a baseURL before you reach deployment will cause errors
+*/
