cda69c1c38876b8229bac1d9238c2b6e8a3fa970
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-05 22:51:27 +0000

    Fetch up and down votes ids in drf inside post data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import NotFound from './components/NotFound';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignIn';
 import PostCreateForm from './pages/posts/PostCreateForm';
+import PostPage from './pages/posts/PostPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path='/signin' render={() => <SignInForm/> }></Route>
           <Route exact path='/signup' render={() => <SignUpForm /> }></Route>
           <Route exact path='/posts/create' render={() => <PostCreateForm />}></Route>
+          <Route exact path='/products/:id' render={() => <PostPage />}></Route>
           <Route render={() => <NotFound />}/>
         </Switch>
       </Container>
--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,34 @@
 from rest_framework import serializers
 from .models import Product
+from votes.models import Vote
 
 class ProductSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     owner = serializers.ReadOnlyField(source = 'owner.username')
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    up_vote_id = serializers.SerializerMethodField()
+    down_vote_id = serializers.SerializerMethodField()
+
+    def get_up_vote_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Vote.objects.filter(
+                # see if currently logged in user is the user who liked the post we are trying to retrieve
+                owner=user, product=obj, up_vote=True
+            ).first()
+            return like.id if like else None
+        return None
     
+    def get_down_vote_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Vote.objects.filter(
+                # see if currently logged in user is the user who liked the post we are trying to retrieve
+                owner=user, product=obj, down_vote=True
+            ).first()
+            return like.id if like else None
+        return None
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -33,5 +55,6 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'category' ,'content', 'image'
+            'title', 'category' ,'content', 'image','up_vote_id',
+            'down_vote_id'
         ]
