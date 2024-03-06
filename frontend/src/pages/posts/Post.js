1edbf533c2e3b5a1d3a75b6f7a76e4f42489e588
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-06 01:43:49 +0000

    Integrate some single post data from drf to frontend

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import styles from "../../styles/Post.module.css"
+import React from "react";
+import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 
 const Post = (props) => {
-  return (
-    <div>Post placeholder sext</div>
-  )
-}
+    const {id, owner, category, category_name, content, created_at, down_vote_id,
+    image, profile_id, profile_image, title, up_vote_id, updated_at,PostPage,} = props
+    
+    const currentUser = useCurrentUser()
+    const is_owner = currentUser?.username === owner
 
-export default Post
+
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && PostPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+        <div className={styles.PostBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : up_vote_id ? (
+            <span onClick={() => {}}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={() => {}}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {/* {likes_count} */}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {/* {comments_count} */}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Post;
