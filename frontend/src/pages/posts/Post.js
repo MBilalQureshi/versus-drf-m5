11050403901ee3e94d18614c084bdb22925c4e1b
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-09 21:39:23 +0000

    Update up, down and delete votes methods

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -25,7 +25,14 @@ const Post = (props) => {
           up_vote:  true,
           down_vote: false, // Set to false as it's an upvote
         };
-        await axiosRes.post('/votes/', postData)
+        const {data} = await axiosRes.post('/votes/', postData)
+        setPosts((prevPosts)=>({
+          ...prevPosts,
+          results: prevPosts.results.map((post) => {
+            return post.id === id
+            ? {...post, up_votes_count: post.up_votes_count + 1, up_vote_id: data.id}: post;
+          })
+        }))
       }catch(err){
         console.log(err)
       }
@@ -35,11 +42,21 @@ const Post = (props) => {
       try{
         let vote_id = 0
         if (up_vote_id === null){
-          vote_id = down_vote_id          
+          vote_id = down_vote_id
         }else{
           vote_id = up_vote_id
         }
         await axiosRes.delete(`/votes/${vote_id}/`)
+        setPosts((prevPosts)=>({
+          ...prevPosts,
+          results: prevPosts.results.map((post) => {
+            return post.id === id && up_vote_id
+            ? {...post, up_votes_count: post.up_votes_count - 1, up_vote_id: null}
+            : post.id === id && down_vote_id
+            ? {...post, down_votes_count: post.down_votes_count - 1, down_vote_id: null}
+            : post;
+          })
+        }))
       }catch(err){
         console.log(err)
       }
@@ -51,7 +68,14 @@ const Post = (props) => {
           up_vote:  false,
           down_vote: true,
         };
-        await axiosRes.post('/votes/', postData)
+        const {data} = await axiosRes.post('/votes/', postData)
+        setPosts((prevPosts)=>({
+          ...prevPosts,
+          results: prevPosts.results.map((post) => {
+            return post.id === id
+            ? {...post, down_votes_count: post.down_votes_count + 1, down_vote_id: data.id}: post;
+          })
+        }))
       }catch(err){
         console.log(err)
       }
@@ -181,8 +205,8 @@ const Post = (props) => {
           <Table striped bordered hover>
             <thead>
               <tr>
-                <th>Up votes</th>
-                <th>Down votes</th>
+                <th>Post Total up votes</th>
+                <th>Post Total Down votes</th>
               </tr>
             </thead>
             <tbody>
