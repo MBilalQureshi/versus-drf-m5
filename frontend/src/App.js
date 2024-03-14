b7ffde459f49e6e8815fd00fece893ed40900a9b
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-14 22:10:39 +0000

    Add basic profile page and route it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import PostPage from './pages/posts/PostPage';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import PostEditForm from './pages/posts/PostEditForm';
+import ProfilePage from './pages/profiles/ProfilePage';
 
 function App() {
   const currentUser = useCurrentUser();
@@ -27,6 +28,7 @@ function App() {
           <Route exact path='/posts/create' render={() => <PostCreateForm />}></Route>
           <Route exact path='/products/posts/:id' render={() => <PostPage />}></Route>
           <Route exact path='/products/posts/:id/edit' render={()=> <PostEditForm />}></Route>
+          <Route exact path='/profiles/:id' render= {() => <ProfilePage />}></Route>
           <Route render={() => <NotFound />}/>
         </Switch>
       </Container>
