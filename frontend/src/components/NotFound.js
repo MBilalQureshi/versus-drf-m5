ae3fbb28db746f06a4b3bf51c5f6da2e92a02e97
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-16 22:26:46 +0000

    Enable not found page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,13 +1,12 @@
 import React from 'react'
-// import NoResults from '../assets/no-results.png'
+import NoResults from '../assets/no-results.png'
 import styles from '../styles/NotFound.module.css'
-// import Asset from '../components/Asset'
+import Asset from '../components/Asset'
 
 const NotFound = () => {
   return (
     <div className={styles.NotFound}>
-        {/* <Asset src={NoResults} message={"Sorry, the page you're looking for doesn't exist"}/> */}
-        <h1>Not found</h1>
+        <Asset src={NoResults} message={"Sorry, the page you're looking for doesn't exist"}/>
     </div>
   )
 }
