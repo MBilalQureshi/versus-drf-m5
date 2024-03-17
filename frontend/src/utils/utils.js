b424404513cd979c31aa221dd8bfca45d33fb8b2
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-17 17:22:29 +0000

    Fix bugs that occured on deployed site

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -5,7 +5,11 @@ export const fetchMoreData = async (resource, setResource) => {
   try {
     let url=new URL(resource.next)
     let correctUrl = resource.next.split(url.origin)[1]
+    const temp = correctUrl.split('/').slice(2);
+    correctUrl = `/${temp.join('/')}`;
+    console.log('correctUrl',correctUrl)
     const { data } = await axiosReq.get(correctUrl);
+    console.log(data);
     setResource((prevResource) => ({
       ...prevResource,
       next: data.next,
