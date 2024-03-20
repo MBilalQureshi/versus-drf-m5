e723a82ffe125e0c306cb34017ee731d0a2a564d
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-20 22:24:47 +0000

    Implement add friends with privacy feature in backend

--- products/views.py
+++ products/views.py
@@ -8,6 +8,8 @@ from django.http import JsonResponse
 from django.db.models import Count, Case, When, IntegerField, Subquery, OuterRef
 from django_filters.rest_framework import DjangoFilterBackend
 from votes.models import Vote
+from friends.models import Friend  # Import the Friend model
+from django.db.models import Q
 
 class ProductList(generics.ListCreateAPIView):
     '''
@@ -19,13 +21,13 @@ class ProductList(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly
     ]
     # queryset = Product.objects.all()
-    up_votes_subquery = Vote.objects.filter(product=OuterRef('pk'), up_vote=True).values('product').annotate(up_vote_count=Count('pk')).values('up_vote_count')
-    # up_votes_count = Coalesce(up_votes_subquery, 0)
-    queryset = Product.objects.annotate(
-        up_votes_count=Coalesce(Subquery(up_votes_subquery, output_field=IntegerField()),0),
-        down_votes_count=Count(Case(When(vote__down_vote=True, then=1), output_field=IntegerField()), distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
+    # up_votes_subquery = Vote.objects.filter(product=OuterRef('pk'), up_vote=True).values('product').annotate(up_vote_count=Count('pk')).values('up_vote_count')
+    # # up_votes_count = Coalesce(up_votes_subquery, 0)
+    # queryset = Product.objects.annotate(
+    #     up_votes_count=Coalesce(Subquery(up_votes_subquery, output_field=IntegerField()),0),
+    #     down_votes_count=Count(Case(When(vote__down_vote=True, then=1), output_field=IntegerField()), distinct=True),
+    #     comments_count=Count('comment', distinct=True)
+    # ).order_by('-created_at')
 
     filter_backends = [
         filters.OrderingFilter,
@@ -57,7 +59,35 @@ class ProductList(generics.ListCreateAPIView):
         "vote__owner__profile",
         'owner__profile',
     ]
+    def get_queryset(self):
+        # Get the current user
+        user = self.request.user
+        up_votes_subquery = Vote.objects.filter(product=OuterRef('pk'), up_vote=True).values('product').annotate(up_vote_count=Count('pk')).values('up_vote_count')
+        
 
+        # Get all products where either privacy is False or the owner is the current user or the owner is a friend
+        if user.is_authenticated:
+            # Get the friends of the current user
+            friends = Friend.objects.filter(owner=user)
+
+            # Get the IDs of the users who are friends with the current user
+            friend_ids = friends.values_list('request__id', flat=True)
+            queryset = Product.objects.filter(
+                Q(privacy=False) | Q(owner=user) | Q(owner_id__in=friend_ids)
+            ).annotate(
+                up_votes_count=Coalesce(Subquery(up_votes_subquery, output_field=IntegerField()),0),
+                down_votes_count=Count(Case(When(vote__down_vote=True, then=1), output_field=IntegerField()), distinct=True),
+                comments_count=Count('comment', distinct=True)
+            ).order_by('-created_at')
+        else:
+            queryset = Product.objects.filter(Q(privacy=False)).annotate(
+                up_votes_count=Coalesce(Subquery(up_votes_subquery, output_field=IntegerField()),0),
+                down_votes_count=Count(Case(When(vote__down_vote=True, then=1), output_field=IntegerField()), distinct=True),
+                comments_count=Count('comment', distinct=True)
+            ).order_by('-created_at')
+
+        return queryset
+        
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
