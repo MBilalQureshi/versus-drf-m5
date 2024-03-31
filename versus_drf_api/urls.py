5322da761cfa660aa48e6f79721253af5c058065
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-03-31 00:38:36 +0000

    Fix extra trailing slash issue and ready for deployment

--- versus_drf_api/urls.py
+++ versus_drf_api/urls.py
@@ -16,19 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from django.views.generic import TemplateView
+# from .views import root_route
 
 urlpatterns = [
-    path('', root_route),
+    # path('', root_route),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/',include('dj_rest_auth.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/dj-rest-auth/',include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('', include('profiles.urls')),
-    path('', include('products.urls')),
-    path('', include('comments.urls')),
-    path('', include('votes.urls')),
-    path('', include('friends.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('products.urls')),
+    path('api/', include('comments.urls')),
+    path('api/', include('votes.urls')),
+    path('api/', include('friends.urls')),
 ]
+handler404 = TemplateView.as_view(template_name='index.html')
